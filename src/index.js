8015a4eaa18308bb4c02450db2dcb63e4587eeb7
Julien Bouquillon <julien@revolunet.com>
2017-10-02 03:03:14 +0200

    prettier

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
-import React from 'react'
-import PropTypes from 'prop-types';
-import ReactDOM from 'react-dom'
-import Spinner from 'react-spinner'
-
+import React from "react"
+import PropTypes from "prop-types"
+import ReactDOM from "react-dom"
+import Spinner from "react-spinner"
 
 const DEFAULT_WAIT_MESSAGE = "Loading in progress..."
 
@@ -15,40 +14,39 @@ const Loading = ({ waitMessage }) => (
 )
 
 const containerStyle = {
-  textAlign: 'center',
-  margin: '2em auto'
+  textAlign: "center",
+  margin: "2em auto"
 }
 
-const loadScript = (url, cb, timeout=1000) => {
+const loadScript = (url, cb, timeout = 1000) => {
   const script = document.createElement("script")
   script.type = "text/javascript"
   script.src = url
-  script.onload = () => setTimeout(() => {
-    // ensure bookeo started
-    var shouldTriggerWidget = axiomct_div === undefined && !axiomct_spinner;
-    //var bookeoContentLoaded = typeof axiomct_div !== "undefined" && axiomct_div && axiomct_div.querySelectorAll('iframe').length;
-    if(shouldTriggerWidget) {
-      axiomct_onload()
-    }
-    if(cb) {
-      cb()
-    }
-  }, timeout)
-  return script;
+  script.onload = () =>
+    setTimeout(() => {
+      // ensure bookeo started
+      var shouldTriggerWidget = axiomct_div === undefined && !axiomct_spinner
+      //var bookeoContentLoaded = typeof axiomct_div !== "undefined" && axiomct_div && axiomct_div.querySelectorAll('iframe').length;
+      if (shouldTriggerWidget) {
+        axiomct_onload()
+      }
+      if (cb) {
+        cb()
+      }
+    }, timeout)
+  return script
 }
 
 class BookeoWidget extends React.Component {
   state = {
-    loading: true,
-    gone: false
+    loading: true
   }
   componentDidMount() {
     this.loadBookeo()
+    this.mounted = true
   }
   componentWillUnmount() {
-    this.setState({
-      gone: true
-    })
+    this.mounted = false
     this.cleanup()
   }
   componentWillReceiveProps(nextProps) {
@@ -61,35 +59,33 @@ class BookeoWidget extends React.Component {
 
   cleanup = () => {
     // cleanup scripts
-    ReactDOM.findDOMNode(this).querySelectorAll("script[src*='bookeo.com']").forEach(node => node.parentElement.removeChild(node));
-    if (typeof axiomct_div !== 'undefined') {
+    ReactDOM.findDOMNode(this)
+      .querySelectorAll("script[src*='bookeo.com']")
+      .forEach(node => node.parentElement.removeChild(node))
+    if (typeof axiomct_div !== "undefined") {
       axiomct_div.parentElement.removeChild(axiomct_div)
-      axiomct_div = undefined;
+      axiomct_div = undefined
     }
-    axiomct_spinner = undefined;
-    axiomct_project = undefined;
-    axiomct_loadStarted = false;
+    axiomct_spinner = undefined
+    axiomct_project = undefined
+    axiomct_loadStarted = false
   }
   loadBookeo = () => {
-    if (!this.state.gone) {
+    if (this.mounted) {
       const script = loadScript(this.props.url, () => {
-        if (!this.state.gone) {
-          this.setState(curState => {
-            if (!curState.gone) {
-              return {
-                loading: false
-              }
-            }
-          });
+        if (this.mounted) {
+          this.setState(() => ({
+            loading: false
+          }))
         }
-      });
+      })
       ReactDOM.findDOMNode(this).appendChild(script)
     }
   }
   render() {
     return (
-      <div style={ containerStyle }>
-        { this.state.loading && <Loading waitMessage={ this.props.waitMessage || DEFAULT_WAIT_MESSAGE } /> }
+      <div style={containerStyle}>
+        {this.state.loading && <Loading waitMessage={this.props.waitMessage || DEFAULT_WAIT_MESSAGE} />}
       </div>
     )
   }
