ae828db68c0e4c079d257be75e54a8472ddd238b
Julien Bouquillon <julien.bouquillon@sg.social.gouv.fr>
2018-06-18 23:57:53 +0200

    prettier + prevent more exceptions on cleanup

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-import React from "react"
-import PropTypes from "prop-types"
-import ReactDOM from "react-dom"
-import Spinner from "react-spinner"
+import React from "react";
+import PropTypes from "prop-types";
+import ReactDOM from "react-dom";
+import Spinner from "react-spinner";
 
-const DEFAULT_WAIT_MESSAGE = "Loading in progress..."
+const DEFAULT_WAIT_MESSAGE = "Loading in progress...";
 
 const Loading = ({ waitMessage }) => (
   <div>
@@ -11,83 +11,94 @@ const Loading = ({ waitMessage }) => (
     <br />
     <Spinner />
   </div>
-)
+);
 
 const containerStyle = {
   textAlign: "center",
   margin: "2em auto"
-}
+};
 
 const loadScript = (url, cb, timeout = 1000) => {
-  const script = document.createElement("script")
-  script.type = "text/javascript"
-  script.src = url
+  const script = document.createElement("script");
+  script.type = "text/javascript";
+  script.src = url;
   script.onload = () =>
     setTimeout(() => {
       // ensure bookeo started
-      var shouldTriggerWidget = axiomct_div === undefined && !axiomct_spinner
+      var shouldTriggerWidget = axiomct_div === undefined && !axiomct_spinner;
       //var bookeoContentLoaded = typeof axiomct_div !== "undefined" && axiomct_div && axiomct_div.querySelectorAll('iframe').length;
       if (shouldTriggerWidget) {
-        axiomct_onload()
+        axiomct_onload();
       }
       if (cb) {
-        cb()
+        cb();
       }
-    }, timeout)
-  return script
-}
+    }, timeout);
+  return script;
+};
 
 class BookeoWidget extends React.Component {
   state = {
     loading: true
-  }
+  };
   componentDidMount() {
-    this.mounted = true
-    this.loadBookeo()
+    this.mounted = true;
+    this.loadBookeo();
   }
   componentWillUnmount() {
-    this.mounted = false
-    this.cleanup()
+    this.mounted = false;
+    this.cleanup();
   }
   componentWillReceiveProps(nextProps) {
     if (this.props.url !== nextProps.url) {
       // reload only if different widget url
-      this.cleanup()
-      this.setState({ loading: true }, this.loadBookeo)
+      this.cleanup();
+      this.setState({ loading: true }, this.loadBookeo);
     }
   }
 
   cleanup = () => {
     // cleanup scripts
-    ReactDOM.findDOMNode(this)
-      .querySelectorAll("script[src*='bookeo.com']")
-      .forEach(node => node && node.parentElement && node.parentElement.removeChild(node))
-    if (typeof axiomct_div !== "undefined") {
-      axiomct_div.parentElement.removeChild(axiomct_div)
-      axiomct_div = undefined
+    try {
+      ReactDOM.findDOMNode(this)
+        .querySelectorAll("script[src*='bookeo.com']")
+        .forEach(
+          node =>
+            node && node.parentElement && node.parentElement.removeChild(node)
+        );
+      if (typeof axiomct_div !== "undefined") {
+        axiomct_div.parentElement.removeChild(axiomct_div);
+        axiomct_div = undefined;
+      }
+      axiomct_spinner = undefined;
+      axiomct_project = undefined;
+      axiomct_loadStarted = false;
+    } catch (e) {
+      console.log(e);
     }
-    axiomct_spinner = undefined
-    axiomct_project = undefined
-    axiomct_loadStarted = false
-  }
+  };
   loadBookeo = () => {
     if (this.mounted) {
       const script = loadScript(this.props.url, () => {
         if (this.mounted) {
           this.setState(() => ({
             loading: false
-          }))
+          }));
         }
-      })
-      ReactDOM.findDOMNode(this).appendChild(script)
+      });
+      ReactDOM.findDOMNode(this).appendChild(script);
     }
-  }
+  };
   render() {
     return (
       <div style={containerStyle}>
-        {this.state.loading && <Loading waitMessage={this.props.waitMessage || DEFAULT_WAIT_MESSAGE} />}
+        {this.state.loading && (
+          <Loading
+            waitMessage={this.props.waitMessage || DEFAULT_WAIT_MESSAGE}
+          />
+        )}
       </div>
-    )
+    );
   }
 }
 
@@ -95,6 +106,6 @@ BookeoWidget.propTypes = {
   // widget url, as defined in bookeo parameters
   url: PropTypes.string.isRequired,
   waitMessage: PropTypes.string
-}
+};
 
-export default BookeoWidget
+export default BookeoWidget;
